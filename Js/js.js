7f99f401b63389ea433bf3dc640041dd5da0c435
iavinash73 <91118866+iavinash73@users.noreply.github.com>
2022-12-15 11:35:00 +0530

    game page's functionalities

---
+++ Js/js.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    $('.dropdown-toggle').dropdown()
+});
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+});
+
+let  content=document.getElementById("avinash").innerHTML;
+console.log(content);
+for(i=0;i<content.length;i++)
+{
+if (content[i]=='1')
+   {
+   let p=document.querySelector(".a a");
+   p.setAttribute("href","");
+   let c=document.querySelector(".a");
+   c.classList.add("done");
+   }
+if (content[i]=='2')
+   {
+   let p=document.querySelector(".b a");
+   p.setAttribute("href","");
+   let c=document.querySelector(".b");
+   c.classList.add("done");
+   
+   } 
+if (content[i]=='3')
+   {
+   let p=document.querySelector(".c a");
+   p.setAttribute("href","");
+   let c=document.querySelector(".c");
+   c.classList.add("done");
+   
+   } 
+if (content[i]=='4')
+   {
+   let p=document.querySelector(".d a");
+   p.setAttribute("href","");
+   let c=document.querySelector(".d");
+   c.classList.add("done");
+   }
+}
+
+// $(document).ready(function(){
+//     $('.hover').popover({
+//         title:fetchdata,
+//         html:true,
+//         placement:right
+//     });
+//     function fetchdata(){
+//         var fetchdata="";
+//         var element=$(this);
+//         var id=element.attr("id");
+//         $ajax({
+//             url:"",
+//             method:'POST',
+//             async:false,
+//             data:{id:id},
+//             success:function(data){
+//                 fetch_data=data;
+//             }
+//         });
+//         return fetch_data;
+//     }
+// });
