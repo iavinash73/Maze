9d84e28ed8ecb2836cc6ca7d79cb162e001441ce
iavinash73 <91118866+iavinash73@users.noreply.github.com>
2023-01-18 09:14:36 +0530

    Update index.js

--- Js/index.js
+++ Js/index.js
@@ -392,4 +392,234 @@ function pathfind(r, c, dir) {
   }
   return;
 }
-
+//prim
+#include <stdio.h>
+#include <stdlib.h>
+struct node 
+{
+    int val;
+    int wei;
+    struct node *next;
+}*newN,*temp,*N,*Min,*t1,*t2,*t3=0,*m1,*m2;
+int l=0,min;
+struct node* newnode() 
+{
+    newN=malloc(sizeof(struct node));
+    return newN;
+};
+void addEdge(struct node*arr[],int n1,int n2,int w)
+{
+    N=newnode();
+    if(arr[n1]==0)
+    {
+        arr[n1]=N;
+        arr[n1]->val=n2;
+        arr[n1]->wei=w;
+    }
+    else
+    {
+        temp=arr[n1];
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=N;
+        temp->next->val=n2;
+        temp->next->wei=w;
+    }
+    if(l==0)
+    {
+        l=w;
+        Min=N;
+    }
+    else if(l>w)
+    {
+        l=w;
+        Min=N;
+    }
+}
+void addEdges(struct node*arr[],int n1,int n2,int w)
+{
+    addEdge(arr,n1,n2,w);
+    addEdge(arr,n2,n1,w);
+}
+struct node * findNode(struct node*arr[],int n,int w)
+{
+    temp=arr[n];
+    while(temp!=NULL)
+    {
+        if(temp->wei==w)
+        {
+            break;
+        }
+        temp=temp->next;
+    }
+    return temp;
+}
+struct node* findNext(struct node* arr[],int n,int check[])
+{
+    temp=arr[n];
+    t3=0;
+    min=0;
+    while(temp!=NULL)
+    {
+        if(min==0&&check[temp->val]!=1)
+        {
+            min=temp->wei;
+            t3=temp;
+        }
+        else if(min>temp->wei&&check[temp->val]!=1)
+        {
+            min=temp->wei;
+            t3=temp;
+        }
+        temp=temp->next;
+    }
+    return t3;
+}
+int main()
+{
+    int n;
+    printf("enter no of vertices: ");
+    scanf("%d",&n);
+    struct node*arr[n];
+    int res[n*2],check[n],ind=0,e=0;
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=0;
+        check[i]=0;
+    } 
+    int edges,n1,n2,w,cost=0;
+    printf("enter no of edges: ");
+    scanf("%d",&edges);
+    while(edges--)
+    {
+        printf("enter two nodes to establish edge: ");
+        scanf("%d %d",&n1,&n2);
+        printf("enter weight: ");
+        scanf("%d",&w);
+        n1--;
+        n2--;
+        addEdges(arr,n1,n2,w); 
+    }
+    t1=Min;
+    t2=findNode(arr,Min->val,l);
+    res[ind++]=t1->val+1;
+    res[ind++]=t2->val+1;
+    check[t1->val]=1;
+    check[t2->val]=1;
+    cost+=t1->wei;
+    while(ind!=n)
+    {
+        m1=findNext(arr,t1->val,check);
+        m2=findNext(arr,t2->val,check);
+        if(m2==0 ||m1->wei<m2->wei)
+        {
+            res[ind++]=m1->val+1;
+            check[m1->val]=1;
+            cost+=m1->wei;
+            t1=m1;
+        }
+        else if(m1==0 ||m1->wei>m2->wei)
+        {
+            res[ind++]=m2->val+1;
+            check[m2->val]=1;
+            cost+=m2->wei;
+            t2=m2;
+        }
+    } 
+    printf(" cost is:%d\n",cost);
+    for(int i=0;i<ind;i++)
+    {
+        printf(" %d",res[i]);
+    }
+}//htdjy
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int n1;
+    int n2;
+    int wei;
+}*N;
+int findNext(struct node*arr[],int n)
+{
+    int min=0,minInd=0;
+    for(int i=1;i<n+1;i++)
+    {
+        if(arr[i]!=NULL)
+        {
+            if(min==0)
+            {
+                min=arr[i]->wei;
+                minInd=i;
+            }
+            else if(min>arr[i]->wei)
+            {
+                min=arr[i]->wei;
+                minInd=i;
+            }
+        }
+    } 
+    return minInd;
+}
+int main()
+{
+    int n,min=0,minInd;
+    printf("enter no of vertices: ");
+    scanf("%d",&n);
+    int check[n+1],res[n+1];
+    for(int i=0;i<n+1;i++)
+    {
+        check[i]=0;
+    }
+    int edges,cost=0,n1,n2,w,ind=0;
+    printf("enter no of edges: ");
+    scanf("%d",&edges);
+    struct node*arr[edges+1];
+    arr[0]=0;
+    for(int i=1;i<edges+1;i++)
+    {
+        printf("enter two nodes to establish edge and enter weight : ");
+        scanf("%d %d %d",&n1,&n2,&w);
+        N=malloc(sizeof(struct node));
+        N->n1=n1;
+        N->n2=n2;
+        N->wei=w;
+        arr[i]=N;
+        if(min==0)
+        {
+            min=w;
+            minInd=i;
+        }
+        else if(min>w)
+        {
+            min=w;
+            minInd=i;
+        }
+    }
+    res[ind++]=arr[minInd]->n1;
+    res[ind++]=arr[minInd]->n2;
+    check[arr[minInd]->n1]=1;
+    check[arr[minInd]->n2]=1;
+    cost+=arr[minInd]->wei;
+    arr[minInd]=NULL;
+    while(ind!=n)
+    {
+        minInd=findNext(arr,edges);
+        if(check[arr[minInd]->n1]!=1||check[arr[minInd]->n2]!=1)
+        {
+            if(check[arr[minInd]->n1]!=1)res[ind++]=arr[minInd]->n1;
+            if(check[arr[minInd]->n2]!=1)res[ind++]=arr[minInd]->n2;
+            check[arr[minInd]->n1]=1;
+            check[arr[minInd]->n2]=1;
+            cost+=arr[minInd]->wei;
+        }
+        arr[minInd]=NULL;
+    }
+    printf(" cost is:%d\n",cost);
+    for(int i=0;i<ind;i++)
+    {
+        printf(" %d",res[i]);
+    }
+}
